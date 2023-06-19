a2ea7ce1719794e907ca261292508885d863a7c4
Chokchai-Fa <phukao1234@gmail.com>
2023-06-19 23:22:14 +0700

    [add] avoid deadlock by handle order of updateing table

--- db/sqlc/store_test.go
+++ db/sqlc/store_test.go
@@ -41,8 +41,6 @@ func TestTransferTx(t *testing.T) {
 		err := <-errs
 		require.NoError(t, err)
 
-		fmt.Print("xxx", results)
-
 		result := <-results
 		require.NotEmpty(t, result)
 
@@ -110,7 +108,58 @@ func TestTransferTx(t *testing.T) {
 	updateAccount2, err := testQueries.GetAccountForUpdate(context.Background(), account2.ID)
 	require.NoError(t, err)
 
-	fmt.Println(">> agter", updateAccount1.Balance, updateAccount2.Balance)
+	fmt.Println(">> after", updateAccount1.Balance, updateAccount2.Balance)
 	require.Equal(t, account1.Balance-int64(n)*amount, updateAccount1.Balance)
-	require.Equal(t, account2.Balance-int64(n)*amount, updateAccount2.Balance)
+	require.Equal(t, account2.Balance+int64(n)*amount, updateAccount2.Balance)
+}
+
+func TestTransferTxDeadLock(t *testing.T) {
+	store := NewStore(testDB)
+
+	account1 := createRandomAccount(t)
+	account2 := createRandomAccount(t)
+	fmt.Println(">> before", account1.Balance, account2.Balance)
+
+	// run n concurrent transfer transactions
+	n := 10
+	amount := int64(10)
+
+	errs := make(chan error)
+
+	for i := 0; i < n; i++ {
+		fromAccountID := account1.ID
+		toAccountID := account2.ID
+
+		if i%2 == 1 {
+			fromAccountID = account2.ID
+			toAccountID = account1.ID
+		}
+
+		go func() {
+			_, err := store.TransferTx(context.Background(), TransferTxParams{
+				FromAccountID: fromAccountID,
+				ToAccountID:   toAccountID,
+				Amount:        amount,
+			})
+
+			errs <- err
+		}()
+	}
+
+	// check results
+	for i := 0; i < n; i++ {
+		err := <-errs
+		require.NoError(t, err)
+	}
+
+	// check the final updated balances
+	updateAccount1, err := testQueries.GetAccountForUpdate(context.Background(), account1.ID)
+	require.NoError(t, err)
+
+	updateAccount2, err := testQueries.GetAccountForUpdate(context.Background(), account2.ID)
+	require.NoError(t, err)
+
+	fmt.Println(">> agter", updateAccount1.Balance, updateAccount2.Balance)
+	require.Equal(t, account1.Balance, updateAccount1.Balance)
+	require.Equal(t, account2.Balance, updateAccount2.Balance)
 }
