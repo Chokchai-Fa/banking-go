c86524b3656cdac35d1ad7d87a5ca15279a9f245
Chokchai-Fa <phukao1234@gmail.com>
2023-06-20 00:37:40 +0700

    change password psql

--- db/sqlc/main_test.go
+++ db/sqlc/main_test.go
@@ -11,7 +11,7 @@ import (
 
 const (
 	dbDriver = "postgres"
-	dbSource = "postgresql://root:@Phukao98765@localhost:5432/simple_bank?sslmode=disable"
+	dbSource = "postgresql://root:Phukao98765@localhost:5432/simple_bank?sslmode=disable"
 )
 
 var testQueries *Queries
