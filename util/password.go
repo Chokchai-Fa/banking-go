d2b4871424a1051f3af95a20931177c3c07ad056
Chokchai-Fa <phukao1234@gmail.com>
2023-06-21 23:57:04 +0700

    [add] unittest for check really math password with custom mathcer

--- util/password.go
+++ util/password.go
@@ -7,7 +7,7 @@ import (
 )
 
 // HashPassword return the bcrypt hash of the password
-func HashedPassword(password string) (string, error) {
+func HashPassword(password string) (string, error) {
 	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
 	if err != nil {
 		return "", fmt.Errorf("failed to hash password: %w", err)
