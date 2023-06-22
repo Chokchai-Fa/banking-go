ac38f3d81242558dd3fe79a845fafef9e3ea7581
Chokchai-Fa <phukao1234@gmail.com>
2023-06-22 22:37:49 +0700

    [edit] token payload

--- token/payload.go
+++ token/payload.go
@@ -15,7 +15,7 @@ var (
 
 // Payload contains the payload data of the token
 type Payload struct {
-	ID        uuid.UUID `json:id`
+	ID        uuid.UUID `json:"id"`
 	Username  string    `json:"username"`
 	IssuedAt  time.Time `json:"issued_at"`
 	ExpiredAt time.Time `json:"expired_at"`
