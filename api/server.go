6329b3a99100c4519e57043ef1c3dbf75752b754
Chokchai-Fa <phukao1234@gmail.com>
2023-06-22 23:30:59 +0700

    [add] login API

--- api/server.go
+++ api/server.go
@@ -2,6 +2,9 @@ package api
 
 import (
 	db "banking-go/db/sqlc"
+	"banking-go/token"
+	"banking-go/util"
+	"fmt"
 
 	"github.com/gin-gonic/gin"
 	"github.com/gin-gonic/gin/binding"
@@ -10,20 +13,38 @@ import (
 
 // Server serves HTTP request for our banking service.
 type Server struct {
-	store  db.Store
-	router *gin.Engine
+	config     util.Config
+	store      db.Store
+	tokenMaker token.Maker
+	router     *gin.Engine
 }
 
 // NewServer creates a new HTTP server and setup routing.
-func NewServer(store db.Store) *Server {
-	server := &Server{store: store}
-	router := gin.Default()
+func NewServer(config util.Config, store db.Store) (*Server, error) {
+	tokenMaker, err := token.NewPasteoMaker(config.TokenSymmetricKey)
+	if err != nil {
+		return nil, fmt.Errorf("cannot create token maker: %w", err)
+	}
+
+	server := &Server{
+		config:     config,
+		store:      store,
+		tokenMaker: tokenMaker,
+	}
 
 	if v, ok := binding.Validator.Engine().(*validator.Validate); ok {
 		v.RegisterValidation("currency", validCurrency)
 	}
 
+	server.setupRouter()
+	return server, nil
+}
+
+func (server *Server) setupRouter() {
+	router := gin.Default()
+
 	router.POST("/users", server.createUser)
+	router.POST("/users/login", server.loginUser)
 
 	router.POST("/accounts", server.createAccount)
 	router.GET("/accounts/:id", server.getAccount)
@@ -32,7 +53,6 @@ func NewServer(store db.Store) *Server {
 	router.POST("/transfers", server.createTransfer)
 
 	server.router = router
-	return server
 }
 
 // Start run HTTP server on a specific address.
--- util/config.go
+++ util/config.go
@@ -1,13 +1,19 @@
 package util
 
-import "github.com/spf13/viper"
+import (
+	"time"
+
+	"github.com/spf13/viper"
+)
 
 // Config stores all configurations of the application.
 // The Values are read by viper from a config file or environment variables.
 type Config struct {
-	DBDriver      string `mapstructure:"DB_DRIVER"`
-	DBSource      string `mapstructure:"DB_SOURCE"`
-	ServerAddress string `mapstructure:"SERVER_ADDRESS"`
+	DBDriver                string        `mapstructure:"DB_DRIVER"`
+	DBSource                string        `mapstructure:"DB_SOURCE"`
+	ServerAddress           string        `mapstructure:"SERVER_ADDRESS"`
+	TokenSymmetricKey       string        `mapstructure:"TOKEN_SYMMETRIC_KEY"`
+	AccessTokenTimeDuration time.Duration `mapstructure:"ACCESS_TOKEN_DURATION"`
 }
 
 // LoadConfig read configuration from file or environment variables.
